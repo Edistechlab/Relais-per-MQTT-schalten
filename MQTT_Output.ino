b7b73b4a0686f453820caaac57ec6cacf23790b8
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2020-04-20 09:44:44 +0200

    Update MQTT_Output.ino

--- MQTT_Output.ino
+++ MQTT_Output.ino
@@ -1,65 +1,45 @@
-/*
-Project:  Output send over MQTT with a ESP8266 / NodeMCU
-Author:   Thomas Edlinger for www.edistechlab.com
-Date:     Created 19.04.2020 
-Version:  V1.0
- 
-Required libraries (Tools -> manage libraries)
- - PubSubClient by Nick O'Leary V2.7.0  
-Required Board (Tools -> Board -> Boards Manager...)
- - Board: esp8266 by ESP8266 Community V2.6.3
-
 Wirering for the Output:
-Relais      NodeMCU
+Relay      NodeMCU
 VCC         3.3V
 GND         G
-SCL         D1 / GPIO5
-SDA         D2 / GPIO4
+IN1         D5 / GPIO14
 */
 
-#include <WiFi.h>
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
 
-#define wifi_ssid "MacWorld"
-#define wifi_password "E2943228373F65C0"
+#define wifi_ssid "Your_SSID"
+#define wifi_password "Your_Wifi_Password"
 
-#define mqtt_server "PiHomeserver.local"
+#define mqtt_server "Your_Server IP"
 #define mqtt_user "your_username"         
 #define mqtt_password "your_password"     
 
-//#define humidity_topic "esp01/humidity"
-//#define temperature_topic "esp01/temperature"
-//#define pressure_topic "esp01/pressure"
+#define relay_topic "esp01/relay"
 #define status_topic "esp01/status" 
 #define inTopic "esp01/output"
-    
-  
+   
 WiFiClient espClient;  
 PubSubClient client(espClient);  
    
 bool status;  
-const int OutputPin = 4;
-
-long lastMsg = 0; ///----
-char msg[50];     ///----
-int value = 0;    ///----
-
+const int OutputPin = 14;  // der Output Pin wo das Relais angehängt ist
     
  void setup() {  
   Serial.begin(115200);   
   setup_wifi();  
   client.setServer(mqtt_server, 1883); 
-  client.setCallback(callback); 
-  if (!client.connected()) {  
-    reconnect();  
-  }  
+  client.setCallback(callback);  
   pinMode(OutputPin, OUTPUT);
-//  client.loop();   
-  
+  digitalWrite(OutputPin, HIGH);
  }  
    
  void loop() {  
+  if (!client.connected()) {  
+    reconnect();  
+  }
+  client.loop();
  }  
    
  void setup_wifi() {  
@@ -91,19 +71,19 @@ int value = 0;    ///----
   }
   Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
   if (String(topic) == "esp01/output") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
       Serial.println("on");
-      //digitalWrite(ledPin, HIGH);
+      digitalWrite(OutputPin, LOW);   //Invertiertes Signal
+      client.publish(relay_topic, "ON");
+      delay(200);
     }
     else if(messageTemp == "off"){
       Serial.println("off");
-      //digitalWrite(ledPin, LOW);
+      digitalWrite(OutputPin, HIGH);  
+      client.publish(relay_topic, "OFF");
+      delay(200);
     }
   }
 }
